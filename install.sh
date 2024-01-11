d9eb7bcae3a1ef080bb00c6a2a038ceacf33c2f0
Howard Huang <hao@hwrd.me>
2024-01-11 15:54:02 -0600

    Conditionally source config if Summit project

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 ln -sf "${PWD}/config" "$HOME/.gitconfig"
+ln -sf "${PWD}/a5.config" "$HOME/.a5.gitconfig"
 ln -sf "${PWD}/ignore" "$HOME/.gitignore_global"
 ln -sf "${PWD}/message" "$HOME/.gitmessage"
